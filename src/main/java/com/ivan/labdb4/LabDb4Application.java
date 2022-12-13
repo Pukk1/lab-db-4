ad045588fe217705f5e0428d3022b835471bd066
Ivan Kustarev <ivan.kustarev@tune-it.ru>
2022-12-13 18:34:29 +0300

    init

---
+++ src/main/java/com/ivan/labdb4/LabDb4Application.java
@@ -0,0 +1,13 @@
+package com.ivan.labdb4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabDb4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabDb4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ivan/labdb4/LabDb4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivan.labdb4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabDb4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
